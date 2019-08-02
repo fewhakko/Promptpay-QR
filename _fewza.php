7da994c61a5fb5046f6e9081d61b58eb77a58bc4
FewHakko <41436874+FewHakko@users.noreply.github.com>
2019-08-02 22:08:11 +0700

    Update _fewza.php

--- _fewza.php
+++ _fewza.php
@@ -3,12 +3,12 @@ require_once("lib/PromptPayQR.php");
 error_reporting(0);
 $response = array();
 
-    if(isset($_GET['qr-code'])) {
+if(isset($_GET['qr-code'])) {
 	if(empty($_POST['percent']) || empty($_POST['money'])) {
         $response["status"] = 'error';
         $response["message"] = 'กรุณาอย่าเว้นช่องว่าง';
     }
-    if (strlen($_POST['percent']) == 13 || strlen($_POST['percent']) == 10) {
+	if (strlen($_POST['percent']) == 13 || strlen($_POST['percent']) == 10) {
 		$PromptPayQR = new PromptPayQR(); // new object
 		$PromptPayQR->size = 8; // Set QR code size to 8
 		$PromptPayQR->id = $_POST['percent']; // PromptPay ID
@@ -16,15 +16,11 @@ $response = array();
 		
 		$response["status"] = "success";
 		$response["base64za"] = $PromptPayQR->generate();
-    }
+	}
     else {
-	$response["status"] = 'error';
+		$response["status"] = 'error';
         $response["message"] = 'ท่านได้กรอกเบอร์หรือเลขบัตรประชาชนไม่ถูกต้อง';
-    }
-}
-
-else {
-		$response["status"] = "error";
+	}
 }
 
 echo json_encode($response);
